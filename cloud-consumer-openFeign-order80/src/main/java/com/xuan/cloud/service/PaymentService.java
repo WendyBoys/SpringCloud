1c97fb6601d8e0cc321dd4ee89e18f8966bdd6e3
小轩 <709027500@qq.com>
2020-06-28 21:51:36 +0800

    SpringCloud

--- cloud-consumer-openFeign-order80/src/main/java/com/xuan/cloud/service/PaymentService.java
+++ cloud-consumer-openFeign-order80/src/main/java/com/xuan/cloud/service/PaymentService.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient("CLOUD-PAYMENT-SERVICE")
+@FeignClient("CLOUD-PAYMENT-SERVICE") //指定服务名
 public interface PaymentService {
 
-    @GetMapping(value = "/payment/get/{id}")
-    CommonResult<Payment> getPaymentById(@PathVariable("id") int id);
+    @GetMapping(value = "/payment/get/{id}") //要和提供者一模一样路径
+    CommonResult<Payment> getPaymentById(@PathVariable("id") int id); //提供者返回的是Payment 这里可以加工返回CommonResult
 
-    @GetMapping(value = "/payment/getTimeoutPort")
+    @GetMapping(value = "/payment/getTimeoutPort") //调用接口时间超过1000ms 应该在ym配置延时超过1000ms 不然会报错
     String getTimeoutPort();
 }
